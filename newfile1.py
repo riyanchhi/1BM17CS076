892f7ea12b6a781a5bb3c1a202a6dcac4a4d3f1a
riyanchhi <53818109+riyanchhi@users.noreply.github.com>
2019-08-06 15:52:26 +0530

    Add files via upload

---
+++ newfile1.py
@@ -0,0 +1,9 @@
+#to detrmine given number is positive, negative and zero
+n=int(input())
+if n > 0:
+    print("positive number")
+elif n < 0:
+    print("negative number")
+else:
+    print("zero")
+        
